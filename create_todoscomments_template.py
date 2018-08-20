73b67a9292cecbdc26c54a6afffe0fbcd812b75f
Tristan AI-Guru <tristan@ai-guru.de>
2018-08-20 18:55:50 +0200

    Minor changes.!

--- create_todoscomments_template.py
+++ create_todoscomments_template.py
@@ -36,38 +36,7 @@ draw.line((center[0], 25, center[0], size[1] - 25), fill="gray", width=1)
 draw_planner(50)
 draw_planner(center[0] + 50)
 
+del draw
+
 output_image.save(output_path)
 print(output_path + " saved.")
-
-
-exit(0)
-
-def draw_box(y1, y2):
-    #step = 54
-    step = 78
-    for x in range(step * 2, size[0] - step, step):
-        draw.line((x, y1, x, y2), fill="gray", width=1)
-
-    draw.line((0, y1, size[0], y1), fill="gray", width=1)
-    draw.line((0, y2, size[0], y2), fill="gray", width=1)
-    draw.line((0, (y1 + y2) // 2, size[0], (y1 + y2) // 2), fill="gray", width=2)
-
-
-draw_box(0, 900)
-draw_box(972, 1872)
-
-def draw_staple(x):
-    offset = 10
-    length = 50
-    x1 = x - offset
-    x2 = x1 - length
-    draw.line((x1, size[1] // 2, x2, size[1] // 2), fill="gray", width=4)
-    x1 = x + offset
-    x2 = x1 + length
-    draw.line((x1, size[1] // 2, x2, size[1] // 2), fill="gray", width=4)
-
-draw_staple(468)
-draw_staple(468 * 2)
-
-
-del draw
